d3b913e59680b18ad69af08d1ad43eafb7308e46
Programmer-Bing <1820754092@qq.com>
2019-09-02 20:22:05 +0800

    增加了一行注释dept实体类中

--- comm/src/main/java/com/cssl/pojo/Dept.java
+++ comm/src/main/java/com/cssl/pojo/Dept.java
@@ -10,6 +10,7 @@ import lombok.experimental.Accessors;
 @Accessors(chain=true)
 //2. chain 一个布尔值。如果为真，产生的setter返回的this而不是void。
 // 默认是假。如果fluent=true，那么chain默认为真。
+//新增注释
 public class Dept {
 
     private Long deptno;
