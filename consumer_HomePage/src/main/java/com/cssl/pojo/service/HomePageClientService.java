6b45ad952e7150d34b42972d635390876f483cf6
Programmer-Bing <1820754092@qq.com>
2019-09-05 16:21:24 +0800

    Merge branch 'master' of E:\Itrip with conflicts.

--- consumer_HomePage/src/main/java/com/cssl/pojo/service/HomePageClientService.java
+++ consumer_HomePage/src/main/java/com/cssl/pojo/service/HomePageClientService.java
@@ -2,6 +2,7 @@ package com.cssl.pojo.service;
 
 import com.cssl.pojo.HomePage_product;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,7 +12,6 @@ import java.util.Map;
 //当一个服务停止了，Consumer客户端提供降级信息
 //@FeignClient("MICROSERVICE-DEPT")
 @FeignClient(value = "MICROSERVICE-DEPT",fallbackFactory=DeptClientServiceFallbackFactory.class)
-@Service
 public interface HomePageClientService {
     @RequestMapping(method = RequestMethod.GET,value="/homeproduct/list")
 	public List<HomePage_product> list();
--- itrip-user/src/main/resources/templates/爱去旅行个人信息主页面_files/userinfo.php
+++
@@ -1,1 +0,0 @@
-trail_getregion_D6DAD5A3({"data":{"province":"湖南","city":"长沙","country":"中国","ip":"42.48.85.216"},"success":true});
