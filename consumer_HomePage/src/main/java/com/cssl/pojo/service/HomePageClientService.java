af3c967d173c98657e043c3f773c9d553e41d2af
Programmer-Bing <1820754092@qq.com>
2019-09-06 17:02:03 +0800

    增加了在主页面显示吃喝玩乐商品的功能

--- consumer_HomePage/src/main/java/com/cssl/pojo/service/HomePageClientService.java
+++ consumer_HomePage/src/main/java/com/cssl/pojo/service/HomePageClientService.java
@@ -14,7 +14,8 @@ import java.util.Map;
 public interface HomePageClientService {
     @RequestMapping(method = RequestMethod.GET,value="findBargains")
     public List<HomePage_product> findBargains(@RequestParam("lp_id") Integer lp_id);
-
+    @RequestMapping(method = RequestMethod.GET,value="findPartyingByCity")
+    public List<HomePage_product> findPartyingByCity(@RequestParam(required = false, value = "P_cid") Integer P_cid, @RequestParam(required = false, value = "Pt_id") Integer Pt_id);
 
     @RequestMapping(method = RequestMethod.GET,value="/homeproduct/list2")
     public List<HomePage_product> list2(@RequestParam Map<String,Object> map);
