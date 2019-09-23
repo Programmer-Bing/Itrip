f23f35f12a118cb6e145cc5f7123187c19d4af3e
ForwarderZB <1640567451@qq.com>
2019-09-23 13:32:33 +0800

    购物车23

---
+++ comm/src/main/java/com/cssl/pojo/smallplate.java
@@ -0,0 +1,13 @@
+package com.cssl.pojo;
+
+import java.util.List;
+
+public class smallplate {
+
+    int sp_id;
+    String sp_name;
+    int lp_id;
+    String picture;
+    List<citiesstates> citi;
+
+}
